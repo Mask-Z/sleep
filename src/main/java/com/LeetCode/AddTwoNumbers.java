89855d8524729b198a90ba45f34006108ab50dc6
Mask-Z <zhyl9248@gmail.com>
2018-04-04 00:16:34 +0800

    TwoSum

---
+++ src/main/java/com/LeetCode/AddTwoNumbers.java
@@ -0,0 +1,53 @@
+package com.LeetCode;
+
+/**
+ * 给定两个链表分别代表两个非负整数，链表的每个结点分别存储整数的每位数字，且是逆序存储，即：数字最低位存储在链表表头，数字最高位存储在链表表尾。求解这两个整数的和并以相同的链表形式返回计算的结果。
+ *
+ * 例如：   输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)   输出：7 -> 0 -> 8
+ */
+public class AddTwoNumbers {
+
+	//Definition for singly-linked list.
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+
+		@Override
+		public String toString() {
+			return "ListNode{" +
+					"val=" + val +
+					'}';
+		}
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+	}
+
+	public static void main(String[] args) {
+		AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+		ListNode l1 = addTwoNumbers.new ListNode(243);
+		ListNode l2 = addTwoNumbers.new ListNode(564);
+		System.out.println(addTwoNumbers.addTwoNumbers(l1,l2));
+	}
+}
---
+++ src/main/java/com/LeetCode/MedianofTwoSortedArrays.java
@@ -0,0 +1,86 @@
+package com.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 有两个大小为 m 和 n 的排序数组 nums1 和 nums2 。
+ * 请找出两个排序数组的中位数并且总的运行时间复杂度为 O(log (m+n)) 。
+ * <p>
+ * 示例 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 中位数是 2.0
+ * <p>
+ * 示例 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * <p>
+ * 中位数是 (2 + 3)/2 = 2.5
+ */
+public class MedianofTwoSortedArrays {
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		for (int i:nums1){
+			list1.add(i);
+		}
+
+		for (int i:nums2){
+			list2.add(i);
+		}
+		list1.addAll(list2);
+		list1.sort(Integer::compare);
+		int size=list1.size();
+		if (size%2==0){
+			return  (list1.get(size / 2) + list1.get(size / 2 - 1))/2.0;
+		}else {
+			return  list1.get(size/2);
+		}
+	}
+
+	//别人的答案
+	//经典分治算法(https://blog.csdn.net/hk2291976/article/details/51107778)
+	public double findMedianSortedArrays(int[] A, int[] B,String overload) {
+		int m = A.length;
+		int n = B.length;
+		if (m > n) { // to ensure m<=n
+			int[] temp = A; A = B; B = temp;
+			int tmp = m; m = n; n = tmp;
+		}
+		int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+		while (iMin <= iMax) {
+			int i = (iMin + iMax) / 2;
+			int j = halfLen - i;
+			if (i < iMax && B[j-1] > A[i]){
+				iMin = iMin + 1; // i is too small
+			}
+			else if (i > iMin && A[i-1] > B[j]) {
+				iMax = iMax - 1; // i is too big
+			}
+			else { // i is perfect
+				int maxLeft = 0;
+				if (i == 0) { maxLeft = B[j-1]; }
+				else if (j == 0) { maxLeft = A[i-1]; }
+				else { maxLeft = Math.max(A[i-1], B[j-1]); }
+				if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+				int minRight = 0;
+				if (i == m) { minRight = B[j]; }
+				else if (j == n) { minRight = A[i]; }
+				else { minRight = Math.min(B[j], A[i]); }
+
+				return (maxLeft + minRight) / 2.0;
+			}
+		}
+		return 0.0;
+	}
+
+	public static void main(String[] args) {
+		MedianofTwoSortedArrays medianofTwoSortedArrays=new MedianofTwoSortedArrays();
+		int[] num1=new int[]{1,2};
+		int[] num2=new int[]{3,4};
+
+		System.out.println(medianofTwoSortedArrays.findMedianSortedArrays(num1,num2,""));
+	}
+}
