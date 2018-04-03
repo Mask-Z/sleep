0d503205f62eb3ec6677a2c914e2d2d156435797
Mask-Z <zhyl9248@gmail.com>
2018-04-03 22:41:05 +0800

    TwoSum

---
+++ src/main/java/com/LeetCode/TwoSum.java
@@ -0,0 +1,80 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数列，找出其中和为特定值的那两个数。
+ * 你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
+ *
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class TwoSum {
+
+	/**
+	 * Approach #1 (Brute Force)
+	 *
+	 * @param integers
+	 * @param target
+	 * @return
+	 */
+	public static Integer[] fun1(Integer[] integers, Integer target) {
+		Integer[] result = new Integer[2];
+		for (int i = 0; i < integers.length - 1; i++) {
+			for (int j = i + 1; j < integers.length; j++) {
+				if (integers[i] + integers[j] == target) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		throw new IllegalArgumentException("no two sum solution");
+	}
+
+	/**
+	 * Approach #2 (Two-pass Hash Table)
+	 *
+	 * @param integers
+	 * @param target
+	 * @return
+	 */
+	public static Integer[] fun2(Integer[] integers, Integer target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < integers.length; i++) {
+			map.put(integers[i], i);
+		}
+		for (int i = 0; i < integers.length; i++) {
+			Integer temp = target - integers[i];
+			if (map.containsKey(temp) && temp != i) {
+				return new Integer[]{i, map.get(temp)};
+			}
+		}
+		throw new IllegalArgumentException("no two sum solution");
+
+	}
+
+	/**
+	 * Approach #3 (One-pass Hash Table)
+	 *
+	 * @param integers
+	 * @param target
+	 * @return
+	 */
+	public static Integer[] fun3(Integer[] integers, Integer target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < integers.length; i++) {
+			Integer temp = target - integers[i];
+			if (map.containsKey(temp)) {
+				return new Integer[]{map.get(temp), i};
+			}
+			map.put(integers[i], i);
+		}
+		throw new IllegalArgumentException("no two sum solution");
+
+	}
+}
---
+++ src/main/test/com/LeetCode/TwoSumTest.java
@@ -0,0 +1,19 @@
+package com.LeetCode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TwoSumTest {
+	@Test
+	public void testFun() {
+		Integer[] integers = new Integer[]{2, 7, 11, 15};
+		Integer target = 9;
+		Integer[] result1 = TwoSum.fun1(integers, target);
+		Integer[] result2 = TwoSum.fun2(integers, target);
+		Integer[] result3 = TwoSum.fun3(integers, target);
+		Arrays.asList(result1).forEach(System.out::print);
+		Arrays.asList(result2).forEach(System.out::print);
+		Arrays.asList(result3).forEach(System.out::print);
+	}
+}
