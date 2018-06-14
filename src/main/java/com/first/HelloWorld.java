039cdba4fe362833b073a71e28bafc1d664f8b39
周云龙 <924806232@qq.com>
2018-06-14 09:42:15 +0800

    ???

---
+++ src/main/java/com/first/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.first;
+
+/**
+ * @author 周云龙
+ * @date 2018/6/14 9:41
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
